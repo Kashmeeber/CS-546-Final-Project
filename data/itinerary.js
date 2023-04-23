e4329201317a35e81fce45d21dde0db2ccb71472
Kashmeeber <areeb.ch2001@gmail.com>
2023-04-23 15:44:05 -0400

    Added addStops function

--- data/itinerary.js
+++ data/itinerary.js
@@ -1,8 +1,9 @@
 import * as d from '../data/index.js';
 import {trips} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb';
+import {getAll, get} from './trips.js';
 
-const createTrip = async (tripId, 
+const createActivity = async (tripId, 
     activityName,
     date,
     startTime,
@@ -12,27 +13,27 @@ const createTrip = async (tripId,
         if(!tripId || !activityName || !date || !startTime || !endTime || !cost){
             throw 'Error: All fields need to have valid values';
           }
-          if(typeof tripId != 'string' || tripId.trim().length == 0){
+          if(typeof tripId !== 'string' || tripId.trim().length === 0){
             throw 'Error: Must provide trip id as valid nonempty string';
           }
           tripId = tripId.trim();
-          if(typeof activityName != 'string' || activityName.trim().length == 0){
+          if(typeof activityName !== 'string' || activityName.trim().length === 0){
             throw 'Error: Must provide activity name as valid nonempty string';
           }
           activityName = activityName.trim();
-          if(typeof date != 'string' || date.trim().length == 0){
+          if(typeof date !== 'string' || date.trim().length === 0){
             throw 'Error: Must provide date as valid nonempty string';
           }
           date = date.trim();
-          if(typeof startTime != 'string' || startTime.trim().length == 0){
+          if(typeof startTime !== 'string' || startTime.trim().length === 0){
             throw 'Error: Must provide start time as valid nonempty string';
           }
           startTime = startTime.trim();
-          if(typeof endTime != 'string' || endTime.trim().length == 0){
+          if(typeof endTime !== 'string' || endTime.trim().length === 0){
             throw 'Error: Must provide end time as valid nonempty string';
           }
           endTime = endTime.trim();
-          if(typeof cost != 'string' || cost.trim().length == 0){
+          if(typeof cost !== 'string' || cost.trim().length === 0){
             throw 'Error: Must provide cost as valid nonempty string';
           }
           cost = cost.trim();
@@ -81,10 +82,63 @@ const createTrip = async (tripId,
            );
          
            if(insertInfo.lastErrorObject.n == 0){
-             throw 'Could not add initnerary';
+             throw 'Could not add intinerary';
            }
              
            let newId = newItinerary["_id"].toString();
            newItinerary["_id"] = newId;
            return newItinerary;
     };
+
+const addStop = async (tripId, stop) => {
+  if (!tripId || !stop) {
+    throw 'Error: All fields need to have valid values';
+  }
+  if(typeof tripId !== 'string' || tripId.trim().length === 0){
+    throw 'Error: Must provide trip id as valid nonempty string';
+  }
+  tripId = tripId.trim();
+  if (!ObjectId.isValid(tripId)) {
+    throw `Error: tripId provided is not a valid ObjectId`;
+  }
+
+  if(typeof stop !== 'string' || stop.trim().length === 0){
+    throw 'Error: Must provide stop as valid nonempty string';
+  }
+  stop = stop.trim();
+  //Need to figure out how what is in stops
+
+  const tripCollection = await trips();
+  const tripInfo = await tripCollection.findOne({_id: new ObjectId(tripId)});
+  if (tripInfo === null) {
+    throw `Error: No band with that id found`;
+  }
+  let stops = tripInfo.stops;
+  for (let y = 0; y < stops.length; y++) {
+    if (stops[y] === stop) {
+      throw `Error: Stop already exists`;
+    }
+  }
+  stops.push(stop);
+  const updatedBand = await bandCollection.updateOne({_id: new ObjectId(tripId)},
+  {$set: {stops: stops}},
+  {returnDocument: 'after'});
+  if (updatedBand.modifiedCount === 0) {
+    throw [500, `Could not update stops of trip with id ${tripId}`];
+  }
+  return `Successfully added ${stop} to trip`;
+};
+
+const removeStop = async (tripId, stop) => {
+
+};
+
+const addToSchedule = async (tripId, intinerary) => {
+
+};
+
+const removeFromSchedule = async (tripId, itinerary) => {
+
+};
+
+export {createActivity, addStop, removeStop, addToSchedule, removeFromSchedule};
